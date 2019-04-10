ddb0663eb9539339d82fdd2c6fb8267149e0a3c7
Jordan Byrd <jordan@baltimorehealthanalytics.com>
2019-04-10 16:44:03 -0400

    while loops

---
+++ whileloop.js
@@ -0,0 +1,40 @@
+console.log("it printed yo")
+var counter = -10
+
+while(counter < 20) {
+    console.log(counter);
+    counter++;
+}
+
+console.log("it printed again yo")
+var counter = 10
+
+while (counter <= 40) {
+    console.log(counter);
+    counter+=2;
+}
+
+// while (counter <= 40) {
+//     if(counter % 2 === 0)
+//     console.log(counter);
+//     counter += 1;
+// }
+
+console.log("ummmmmmmmmmmmmmm")
+var counter = 300;
+
+while (counter <= 333) {
+    if (counter % 2 === 0)
+        console.log(counter);
+    counter += 1;
+}
+
+console.log("burr!!")
+var counter = 5;
+
+while (counter <= 50) {
+    if(counter % 5 === 0 && counter % 3 === 0) {
+        console.log(counter);
+    }
+    counter+= 1;
+}
